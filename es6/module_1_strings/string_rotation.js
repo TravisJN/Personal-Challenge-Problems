9fd4a89b75a82b81155eecefac9759ef9217dd43
travisjn <tdizzletrav@gmail.com>
2015-07-03 14:43:10 -0700

    Complete stringRotation

--- es6/module_1_strings/string_rotation.js
+++ es6/module_1_strings/string_rotation.js
@@ -5,6 +5,23 @@
 //Example: stringRotation("thomas", "smotha");-> false
 
 let stringRotation = (str1, str2) => {
+  // Start with first letter of rotated string and iterate from there
+  // find index of first rotated letter in string1 and set that as the start
+  // iterate through string1 and string2 together checking if each letter is equal
+  // if you get to the end of string1, reset index to 0 and continue
+  let array1 = str1.split('');
+  let array2 = str2.split('');
+  let index1 = array1.indexOf(array2[0]);
 
+  for(let item of array2){
+    if (item !== array1[index1++]) {
+      return false;
+    }
+    if(index1 === array1.length){
+      index1 = 0;
+    }
+  }
+
+  return true;
 };
 
